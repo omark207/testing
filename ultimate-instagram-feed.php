0c2d21fd979cda782f3522b8abecda74e53bcf28
Personal Access Token <omar.kasem207@gmail.com>
2022-09-21 18:36:03 +0200

    hee

--- ultimate-instagram-feed.php
+++ ultimate-instagram-feed.php
@@ -21,7 +21,7 @@
  * Author URI:        https://profiles.wordpress.org/omarkasem
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       ultimate-instagram-feed
+ * Text Domain:       ultimate-instagram-feedzzz
  * Domain Path:       /languages
  */
 
